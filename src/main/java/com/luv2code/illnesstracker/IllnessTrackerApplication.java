7a15d2c2458a45b44b76ac6007238e8c9a7524d6
Luka Žugaj <luka.zugaj7@gmail.com>
2020-11-29 12:39:29 +0100

    Initial commit

---
+++ src/main/java/com/luv2code/illnesstracker/IllnessTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code.illnesstracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IllnessTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IllnessTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/luv2code/illnesstracker/IllnessTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.illnesstracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IllnessTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
