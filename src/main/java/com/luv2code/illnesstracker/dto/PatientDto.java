6cbba5b229ac8b659ed4962bb21e60bc0870a7a8
Luka Žugaj <luka.zugaj7@gmail.com>
2020-12-06 18:19:30 +0100

    Implement patient logic
    Add dto and mapper

---
+++ src/main/java/com/luv2code/illnesstracker/dto/PatientDto.java
@@ -0,0 +1,27 @@
+package com.luv2code.illnesstracker.dto;
+
+import com.luv2code.illnesstracker.domain.GenderType;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class PatientDto {
+
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String phoneNumber;
+
+    private String oib;
+
+    private LocalDate dateOfBirth;
+
+    private GenderType gender;
+
+}
