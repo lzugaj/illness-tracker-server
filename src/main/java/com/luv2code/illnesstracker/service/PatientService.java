8415f6e77008ffa9fea26393ec56b7b2eaf83b0b
Luka Žugaj <luka.zugaj7@gmail.com>
2020-12-06 18:18:34 +0100

    Implement patient logic
    Add services and implementation

---
+++ src/main/java/com/luv2code/illnesstracker/service/PatientService.java
@@ -0,0 +1,19 @@
+package com.luv2code.illnesstracker.service;
+
+import com.luv2code.illnesstracker.domain.Patient;
+
+import java.util.List;
+
+public interface PatientService {
+
+    Patient save(final Patient patient);
+
+    Patient findById(final Long id);
+
+    List<Patient> findAll();
+
+    Patient update(final Patient oldPatient, final Patient newPatient);
+
+    void delete(final Patient patient);
+
+}
